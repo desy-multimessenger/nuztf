f2dc68ddd0aee1bd2d38ace5a41357609a88f268
Robert Stein <robert.stein@desy.de>
2019-09-02 19:31:18 +0200

    Stress-test with more history

--- gw_multi_process.py
+++ gw_multi_process.py
@@ -18,7 +18,7 @@ class MultiGwProcessor(GravWaveScanner):
     queue = None
     results = dict()
 
-    def __init__(self, n_cpu, **kwargs):
+    def __init__(self, n_cpu, id=0, **kwargs):
         GravWaveScanner.__init__(self, **kwargs)
         self.cache_dir = os.path.join(
             ligo_candidate_cache,
@@ -30,7 +30,10 @@ class MultiGwProcessor(GravWaveScanner):
             pass
         self.scan_radius = np.degrees(hp.max_pixrad(self.cone_nside))
         self.queue = JoinableQueue()
-        self.processes = [Process(target=self.scan_wrapper) for _ in range(int(n_cpu))]
+        self.processes = [Process(target=self.scan_wrapper, kwargs={"id": i+1}) for i in range(int(n_cpu))]
+
+        self.obj_names = []
+        self.mp_id = id
 
         for p in self.processes:
             p.start()
@@ -38,7 +41,9 @@ class MultiGwProcessor(GravWaveScanner):
     def add_to_queue(self, item):
         self.queue.put(item)
 
-    def scan_wrapper(self):
+    def scan_wrapper(self, **kwargs):
+        self.mp_id = kwargs["id"]
+
         while True:
             ztf_object = self.queue.get()
             if ztf_object is None:
@@ -47,29 +52,34 @@ class MultiGwProcessor(GravWaveScanner):
             query_res = [x for x in ztf_object]
             res = self.filter(query_res)
 
-            self.dump_cache(res)
+            self.obj_names += [x["objectId"] for x in res]
+
+            # self.dump_cache(res)
             self.queue.task_done()
+
+        self.dump_cache()
 
     def filter(self, query_res):
 
         indexes = []
 
         for i, res in enumerate(query_res):
-            if self.fast_filter_f_no_prv(res):
+            # print(i)
+            if self.filter_f_no_prv(res):
                 if self.filter_ampel(res) is not None:
                     indexes.append(i)
 
         return [query_res[i] for i in indexes]
 
-    def fast_filter_f_no_prv(self, res):
+    def filter_f_no_prv(self, res):
 
         # Positive detection
         if res['candidate']['isdiffpos'] not in ["t", "1"]:
             return False
 
         # Veto old transients
-        if res["candidate"]["jdstarthist"] < self.t_min.jd:
-            return False
+        # if res["candidate"]["jdstarthist"] < self.t_min.jd:
+        #     return False
 
         # # Check contour
         # if not self.in_contour(res["candidate"]["ra"], res["candidate"]["dec"]):
@@ -77,23 +87,34 @@ class MultiGwProcessor(GravWaveScanner):
 
         return True
 
-    def dump_cache(self, res):
-        for obj in res:
-            path = os.path.join(self.cache_dir, obj["objectId"]+".pkl")
-            with open(path, "wb") as f:
-                pickle.dump(obj, f)
+    # def dump_cache(self, res):
+    #     for obj in res:
+    #         path = os.path.join(self.cache_dir, obj["objectId"] + ".pkl")
+    #         with open(path, "wb") as f:
+    #             pickle.dump(obj, f)
+    def dump_cache(self):
 
-    def fill_queue(self):
+        path = os.path.join(self.cache_dir, "{0}.pkl".format(self.mp_id))
+
+        with open(path, "rb") as f:
+            pickle.dump(self.obj_names, f)
+
+    def fill_queue(self, max_cones=None):
+
+        if max_cones is None:
+            max_cones = len(self.cone_ids)
 
         t_max = self.default_t_max
 
-        for i, cone_id in enumerate(tqdm(list(self.cone_ids))):
-            self.scanned_pixels.append(cone_id)
-            ra, dec = self.cone_coords[i]
-            ztf_object = ampel_client.get_alerts_in_cone(
-                ra, dec, self.scan_radius, self.t_min.jd, t_max.jd, with_history=False)
-            query_res = [x for x in ztf_object]
-            r.add_to_queue(query_res)
+        for k, cone_id in enumerate(tqdm(list(self.cone_ids)[:max_cones])):
+            if cone_id not in self.scanned_pixels:
+                ra, dec = self.cone_coords[k]
+                ztf_object = ampel_client.get_alerts_in_cone(
+                    np.degrees(ra), np.degrees(dec), self.scan_radius, self.t_min.jd-10, t_max.jd, with_history=False)
+                query_res = [x for x in ztf_object]
+                # print(query_res, ra, dec)
+                r.add_to_queue(query_res)
+                self.scanned_pixels.append(cone_id)
 
     def terminate(self):
         """ wait until queue is empty and terminate processes """
@@ -102,20 +123,20 @@ class MultiGwProcessor(GravWaveScanner):
             p.terminate()
 
     def combine_cache(self):
-        self.cache = dict()
 
         for name in self.get_cache_file():
             try:
                 with open(os.path.join(self.cache_dir, name), "rb") as f:
-                    obj = pickle.load(f)
-                    self.cache[obj["objectId"]] = obj
+                    self.obj_names += pickle.load(f)
             except:
                 pass
 
-        print("Scanned {0} pixels".format(len(self.scanned_pixels)))
-        print("Found {0} candidates".format(len(self.cache)))
+        self.obj_names = list(set(self.obj_names))
 
-        self.create_candidate_summary()
+        print("Scanned {0} pixels".format(len(self.scanned_pixels)))
+        print("Found {0} candidates".format(len(self.obj_names)))
+
+        # self.create_candidate_summary()
 
     def get_cache_file(self):
         return [os.path.join(self.cache_dir, x) for x in os.listdir(self.cache_dir) if ".pkl" in x]
@@ -135,14 +156,15 @@ if __name__ == '__main__':
     logger.setLevel(logging.ERROR)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--n_cpu", default=max(1, os.cpu_count()-1))
+    parser.add_argument("-n", "--n_cpu", default=min(24, max(1, os.cpu_count()-1)))
+    parser.add_argument("-p", "--prob_threshold", default=0.9)
     cfg = parser.parse_args()
 
     print("N CPU available", os.cpu_count())
 
-    r = MultiGwProcessor(n_cpu=cfg.n_cpu, logger=logger, fast_query=True)
+    r = MultiGwProcessor(n_cpu=cfg.n_cpu, logger=logger, prob_threshold=cfg.prob_threshold, fast_query=True)
     r.clean_cache()
-    r.fill_queue()
+    r.fill_queue(max_cones=100)
     r.terminate()
     r.combine_cache()
-    r.clean_cache()
+    # r.clean_cache()
