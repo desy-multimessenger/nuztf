685d18b98dd07f068f1ed80b6b0d652843288dbd
13aaa0fb5ae7d3333b90c4f5f8e7be455c0191dd <necker@physik.hu-berlin.de>
2021-06-02 16:05:12 +0200

    set up ampel credentials in ampel_magic.py

--- ampel_magic.py
+++ ampel_magic.py
@@ -107,6 +107,19 @@ class AmpelWizard:
         if not hasattr(self, "dist"):
             self.dist = None
 
+        try:
+            io.set_account("ampel_api",
+                           username=os.environ["AMPEL_API_USER"],
+                           password=os.environ["AMPEL_API_PASSWORD"])
+            logger.info('Set up AMPEL credentials')
+
+        except KeyError:
+            logger.info('No Credentials for AMPEL API found in environment'
+                        'Assume they are set.')
+
+        if not os.path.exists(io.LOCALSOURCE):
+            os.mkdir(io.LOCALSOURCE)
+
         self.api_user, self.api_pass = io._load_id_("ampel_api")
 
     def get_name(self):
