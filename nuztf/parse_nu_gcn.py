bd71b6677d339ce6519a22a5fefa0fd610c2af42
Robert Stein <robert.stein@desy.de>
2021-09-10 11:09:52 +0200

    Unify all gcn parser scripts in one place

--- nuztf/parse_nu_gcn.py
+++ nuztf/parse_nu_gcn.py
@@ -1,7 +1,7 @@
 import requests
 import numpy as np
-from ztf_plan_obs.gcn_parser import parse_gcn_circular
-
+import re
+from astropy.time import Time
 
 base_gcn_url = "https://gcn.gsfc.nasa.gov/gcn3"
 
@@ -98,3 +98,64 @@ def get_latest_gcn():
     latest = parse_gcn_archive()[0]
     print(f"Latest GCN is {latest[0]} (GCN #{latest[1]})")
     return latest[1]
+
+
+def parse_radec(str):
+    regex_findall = re.findall(r"[-+]?\d*\.\d+|\d+", str)
+    if len(regex_findall) == 4:
+        pos = float(regex_findall[0])
+        pos_upper = float(regex_findall[1])
+        pos_lower = float(regex_findall[1])
+    elif len(regex_findall) == 5:
+        pos, pos_upper, pos_lower = regex_findall[0:3]
+        pos = float(pos)
+        pos_upper = float(pos_upper.replace("+", ""))
+        pos_lower = float(pos_lower.replace("-", ""))
+    else:
+        raise ParsingError(f"Could not parse GCN ra and dec")
+
+    return pos, pos_upper, pos_lower
+
+
+def parse_gcn_circular(gcn_number):
+    url = f"https://gcn.gsfc.nasa.gov/gcn3/{gcn_number}.gcn3"
+    response = requests.get(url)
+    returndict = {}
+    mainbody_starts_here = 999
+    splittext = response.text.splitlines()
+    splittext = list(filter(None, splittext))
+    for i, line in enumerate(splittext):
+        if "SUBJECT" in line:
+            name = line.split(" - ")[0].split(": ")[1]
+            returndict.update({"name": name})
+        elif "FROM" in line:
+            base = line.split("at")[0].split(": ")[1].split(" ")
+            author = [x for x in base if x != ""][1]
+            returndict.update({"author": author})
+        elif (
+            ("RA" in line or "Ra" in line)
+            and ("DEC" in splittext[i + 1] or "Dec" in splittext[i + 1])
+            and i < mainbody_starts_here
+        ):
+            ra, ra_upper, ra_lower = parse_radec(line)
+            dec, dec_upper, dec_lower = parse_radec(splittext[i + 1])
+            ra_err = [ra_upper, -ra_lower]
+            dec_err = [dec_upper, -dec_lower]
+            returndict.update(
+                {"ra": ra, "ra_err": ra_err, "dec": dec, "dec_err": dec_err}
+            )
+            mainbody_starts_here = i + 2
+        elif ("Time" in line or "TIME" in line) and i < mainbody_starts_here:
+            raw_time = [
+                x for x in line.split(" ") if x not in ["Time", "", "UT", "UTC"]
+            ][1]
+            raw_time = "".join(
+                [x for x in raw_time if np.logical_or(x.isdigit(), x in [":", "."])]
+            )
+            raw_date = name.split("-")[1][:6]
+            ut_time = f"20{raw_date[0:2]}-{raw_date[2:4]}-{raw_date[4:6]}T{raw_time}"
+            time = Time(ut_time, format="isot", scale="utc")
+            returndict.update({"time": time})
+
+    return returndict
+
