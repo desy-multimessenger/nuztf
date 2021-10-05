ef029ecddeb824901ef9d5f818a3d4284815a79e
Robert Stein <robert.stein@desy.de>
2021-10-05 15:59:29 +0200

    Deduplicate query code for TNS

--- nuztf/cat_match.py
+++ nuztf/cat_match.py
@@ -1,7 +1,4 @@
-import backoff
-import requests
-from json import JSONDecodeError
-from nuztf.ampel_api import ampel_api_catalog, API_CATALOGMATCH_URL
+from nuztf.ampel_api import ampel_api_catalog
 
 
 def query_ned_for_z(ra_deg: float, dec_deg: float, searchradius_arcsec: float = 20):
@@ -24,43 +21,24 @@ def query_ned_for_z(ra_deg: float, dec_deg: float, searchradius_arcsec: float = 
     return z, dist_arcsec
 
 
-@backoff.on_exception(
-    backoff.expo,
-    requests.exceptions.RequestException,
-    max_time=600,
-)
 def ampel_api_tns(ra_deg: float, dec_deg: float, searchradius_arcsec: float = 3):
     """Function to query TNS via ampel api"""
-    queryurl_catalogmatch = API_CATALOGMATCH_URL + f"/cone_search/nearest"
-
-    # First, we create a json body to post
-    headers = {"accept": "application/json", "Content-Type": "application/json"}
-    query = {
-        "ra_deg": ra_deg,
-        "dec_deg": dec_deg,
-        "catalogs": [
-            {"name": "TNS", "rs_arcsec": searchradius_arcsec, "use": "extcats"}
-        ],
-    }
-
-    # Now we retrieve results from the API
-    response = requests.post(url=queryurl_catalogmatch, json=query, headers=headers)
-
-    if response.status_code == 503:
-        raise requests.exceptions.RequestException
 
     full_name = None
     discovery_date = None
     source_group = None
 
-    try:
-        res = response.json()
-    except JSONDecodeError:
-        print(response)
-        raise Exception
+    res = ampel_api_catalog(
+        catalog="TNS",
+        catalog_type="extcats",
+        ra_deg=ra_deg,
+        dec_deg=dec_deg,
+        searchradius_arcsec=searchradius_arcsec,
+        searchtype="nearest",
+    )
 
-    if res[0]:
-        response_body = res[0]["body"]
+    if res:
+        response_body = res["body"]
         name = response_body["objname"]
         prefix = response_body["name_prefix"]
         full_name = prefix + name
@@ -88,8 +66,8 @@ def get_cross_match_info(raw):
     )
     if res is not None:
         if len(res) == 1:
-            label = f"MILLIQUAS: '{res[0]['body']['broad_type']}'-type source ({res[0]['dist_arcsec']:.2f} arsec)"
+            label = f"[MILLIQUAS: '{res[0]['body']['broad_type']}'-type source ({res[0]['dist_arcsec']:.2f} arsec)]"
         else:
-            label = "MULTIPLE MILLIQUAS MATCHES"
+            label = "[MULTIPLE MILLIQUAS MATCHES]"
 
     return label
