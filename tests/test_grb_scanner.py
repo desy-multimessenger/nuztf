9738ab13c23b56ff6794a2063293c30b379630a5
simeonreusch <simeon.reusch@desy.de>
2021-12-06 12:31:05 +0100

    changed logging level for unittests

--- tests/test_grb_scanner.py
+++ tests/test_grb_scanner.py
@@ -7,23 +7,25 @@ class TestGRBScanner(unittest.TestCase):
 
     maxDiff = None
 
-    def test_scan(self):
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.INFO)
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
 
-        logger.info("\n\n Testing GRB Scanner \n\n")
+    def test_scan(self):
+
+        self.logger.info("\n\n Testing GRB Scanner \n\n")
 
         grb_name = "GRB210927A"
         prob_threshold = 0.9
 
-        logging.info(f"Scanning with GRB {grb_name}")
+        self.logger.info(f"Scanning with GRB {grb_name}")
 
         scanner = SkymapScanner(
             event_name=grb_name,
             scan_mode="grb",
             prob_threshold=prob_threshold,
             n_days=0.1,
-            logger=logger,
+            logger=self.logger,
         )
 
         scanner.get_alerts()
@@ -31,7 +33,7 @@ class TestGRBScanner(unittest.TestCase):
         n_retrieved_alerts = scanner.n_alerts
         n_expected_alerts = 6356
 
-        logging.info(
+        self.logger.info(
             f"Retrieved {n_retrieved_alerts} alerts. {n_expected_alerts} alerts expected."
         )
 
@@ -42,7 +44,7 @@ class TestGRBScanner(unittest.TestCase):
         n_retrieved_candidates = len(scanner.final_candidates)
         n_expected_candidates = 3
 
-        logging.info(
+        self.logger.info(
             f"Retrieved {n_retrieved_candidates} candidates. {n_expected_candidates} candidates expected."
         )
 
