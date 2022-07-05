5b7996474a5be65ffb0df1494edb9b35921fd78f
simeonreusch <simeon.reusch@desy.de>
2022-07-05 17:47:18 +0200

    try to bump astropy

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "ampel-interface == 0.7.1",
         "ampel-photometry == 0.7.1",
         "ampel-ztf == 0.7.4",
-        "astropy == 4.3.1",
+        "astropy == 5.1",
         "black == 22.1.0",
         "backoff == 1.11.1",
         "coveralls == 3.3.1",
