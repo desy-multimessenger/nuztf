bfdfe114a4f7ac51f5e14447289e5bd8e213acfd
simeonreusch <simeon.reusch@desy.de>
2023-04-04 13:26:42 +0200

    adding first version of slackbot server file

---
+++ slackbot_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Simeon Reusch (simeon.reusch@desy.de)
+
+import os
+
+from flask import Flask  # type: ignore
+from slack import WebClient  # type: ignore
+from slackeventsapi import SlackEventAdapter  # type: ignore
+
+nuztf_slackbot = Flask(__name__)
+
+slack_events_adapter = SlackEventAdapter(
+    os.environ.get("SLACK_EVENTS_TOKEN"), "/slack/events", nuztf_slackbot
+)
+slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN_NUZTF"))
