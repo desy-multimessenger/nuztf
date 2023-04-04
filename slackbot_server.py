b546e9c3028584f14e3c8e07d1a1def4db26b3b7
simeonreusch <simeon.reusch@desy.de>
2023-04-04 13:48:36 +0200

    adding systemd service template

--- slackbot_server.py
+++ slackbot_server.py
@@ -10,6 +10,6 @@ from slackeventsapi import SlackEventAdapter  # type: ignore
 nuztf_slackbot = Flask(__name__)
 
 slack_events_adapter = SlackEventAdapter(
-    os.environ.get("SLACK_EVENTS_TOKEN"), "/slack/events", nuztf_slackbot
+    os.environ.get("SLACK_EVENTS_TOKEN"), "/slackbot_nuztf/events", nuztf_slackbot
 )
 slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN_NUZTF"))
