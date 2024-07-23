cb2b93ce4e0370809f76b0c3e59c996f7bf7a57b
JannisNe <necker@physik.hu-berlin.de>
2024-07-23 21:57:39 +0200

    BUGFIX: make sure to include all pixels in the rectangle

--- nuztf/neutrino_scanner.py
+++ nuztf/neutrino_scanner.py
@@ -222,10 +222,10 @@ class NeutrinoScanner(BaseScanner):
 
         center_ra = np.radians(np.mean([self.ra_max, self.ra_min]))
         center_dec = np.radians(np.mean([self.dec_max, self.dec_min]))
-        rad = (
-            np.radians(max(self.ra_max - self.ra_min, self.dec_max - self.dec_min))
-            / 2.0
-        )
+        # Take the larger of the two sides and convert to radians
+        # To make sure to include all pixels until the edge of the rectangle, we have to devide by sqrt(2)
+        # (not 2 as previously done here!)
+        rad = np.radians(max(self.ra_max - self.ra_min, self.dec_max - self.dec_min)) / np.sqrt(2)
 
         nearish_pixels = list(
             hp.query_disc(
