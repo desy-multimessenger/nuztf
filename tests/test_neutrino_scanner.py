bfccc291942388eb1568d2a1ece048987f9565b6
JannisNe <necker@physik.hu-berlin.de>
2025-11-21 13:59:54 +0100

    nicer plot

--- tests/test_neutrino_scanner.py
+++ tests/test_neutrino_scanner.py
@@ -176,7 +176,7 @@ class TestNeutrinoScanner(unittest.TestCase):
                 # radius="5 deg",
             )
             _t = ax.get_transform("world")
-            # ax.imshow_hpx(filename)
+            ax.imshow_hpx(filename)
             ax.contour_hpx(nu_kafka.credible_levels, levels=[0.9], colors="C0")
             gcn_rect = Quadrangle(
                 [left_lower_corner_ra, left_lower_corner_dec],
@@ -195,7 +195,7 @@ class TestNeutrinoScanner(unittest.TestCase):
                 c = f"C{2 + i}"
                 ax.add_patch(Polygon(vert, transform=_t, edgecolor=c, facecolor=c, ls="-", alpha=0.2))
                 ax.plot([], [], color=c, label=f"Field {fields[i]}")
-            ax.legend(loc="upper right")
+            ax.legend(loc="upper right", ncol=2)
 
             ax.set_xlabel("RA")
             ax.set_ylabel("Dec")
