4d07e1ab2a829e4e15a38614799e8e50abce8e4e
JannisNe <necker@physik.hu-berlin.de>
2026-06-16 10:51:19 +0200

    black and isort

--- nuztf/base_scanner.py
+++ nuztf/base_scanner.py
@@ -21,8 +21,8 @@ from astropy.time import Time
 from matplotlib.backends.backend_pdf import PdfPages
 from tqdm import tqdm
 
-from nuztf.ampel.utils import merge_alerts
 from nuztf.ampel.urls import API_CATALOGMATCH_URL
+from nuztf.ampel.utils import merge_alerts
 from nuztf.api import ZTF_BACKEND, api_name, api_skymap
 from nuztf.cat_match import ampel_api_tns, get_cross_match_info, query_ned_for_z
 from nuztf.flatpix import get_nested_pix
@@ -69,7 +69,7 @@ class BaseScanner:
 
         if resource is None:
             resource = {
-                "ampel-ztf/catalogmatch":API_CATALOGMATCH_URL,
+                "ampel-ztf/catalogmatch": API_CATALOGMATCH_URL,
             }
 
         if logger is None:
